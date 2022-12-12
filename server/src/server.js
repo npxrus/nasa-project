c01d3314ee2ba7f8f0d8da78f0819bb3de1c3060
npxrus <npxrus@yandex.com>
2022-12-12 19:51:27 +0300

    feat: add mongo schema for launches

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,22 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 5500;
+const MONGO_URL = 'mongodb://127.0.0.1:27017/nasa';
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () =>
+  console.log('MongoDB connection ready!')
+);
+
+mongoose.connection.on('error', (err) => console.error(err));
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
