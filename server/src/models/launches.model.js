c53b25f2a6152084694ddd528e45c07702b6d0d5
npxrus <npxrus@yandex.com>
2022-12-13 19:07:23 +0300

    feat: add launches to database

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,4 @@
-// const launches = require('./launches.mongo');
+const launchesDb = require('./launches.mongo');
 
 const launches = new Map();
 
@@ -15,14 +15,20 @@ const launch = {
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
 function isLaunchExists(launchId) {
   return launches.has(launchId);
 }
 
-function getAllLaunches() {
-  return Array.from(launches.values());
+async function getAllLaunches() {
+  return await launchesDb.find({}, { _id: 0, __v: 0 });
+}
+
+async function saveLaunch(launch) {
+  await launchesDb.updateOne({ flightNumber: launch.flightNumber }, launch, {
+    upsert: true,
+  });
 }
 
 function addNewLaunch(launch) {
